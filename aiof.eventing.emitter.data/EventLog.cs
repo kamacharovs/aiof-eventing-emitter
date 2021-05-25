fce7401472e61e2ac5ab5b7eb407f2b1f7f8c796
Georgi Kamacharov <gkamacharov@gmail.com>
2021-05-25 10:03:27 -0400

    add unit tests, helper method and automapper for eventlog

---
+++ aiof.eventing.emitter.data/EventLog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Azure.Cosmos.Table;
+
+namespace aiof.eventing.emitter.data
+{
+    public class EventLog : TableEntity
+    {
+        public EventLog() { }
+
+        public EventLog(string eventType, string eventId)
+        {
+            PartitionKey = eventType;
+            RowKey = eventId;
+        }
+
+        public string SourceApi { get; set; }
+        public string SourceIp { get; set; }
+        public int? UserId { get; set; }
+        public Guid? UserPublicKey { get; set; }
+    }
+}
--- aiof.eventing.emitter.data/EventProfile.cs
+++ aiof.eventing.emitter.data/EventProfile.cs
@@ -10,7 +10,17 @@ namespace aiof.eventing.emitter.data
     {
         public EventProfile()
         {
+            CreateMap<EventRequest, EventLog>()
+                .ForMember(x => x.PartitionKey, o => o.MapFrom(s => s.EventTypeEnum.ToString()))
+                .ForMember(x => x.RowKey, o => o.MapFrom(s => s.EventId));
 
+            CreateMap<EventSource, EventLog>()
+                .ForMember(x => x.SourceApi, o => o.Condition(s => !string.IsNullOrWhiteSpace(s.Api)))
+                .ForMember(x => x.SourceIp, o => o.Condition(s => !string.IsNullOrWhiteSpace(s.Ip)));
+
+            CreateMap<EventUser, EventLog>()
+                .ForMember(x => x.UserId, o => o.Condition(s => s.Id.HasValue))
+                .ForMember(x => x.UserPublicKey, o => o.Condition(s => s.PublicKey.HasValue));
         }
     }
 }
