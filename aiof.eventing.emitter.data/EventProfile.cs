a251cec4973a5682745ae356bb9ed6b837d869e2
Georgi Kamacharov <gkamacharov@gmail.com>
2021-05-27 11:14:54 -0400

    update to cleanup

--- aiof.eventing.emitter.data/EventProfile.cs
+++ aiof.eventing.emitter.data/EventProfile.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-//using System.Text.Json;
 
 using Newtonsoft.Json;
-
 using AutoMapper;
 
 namespace aiof.eventing.emitter.data
